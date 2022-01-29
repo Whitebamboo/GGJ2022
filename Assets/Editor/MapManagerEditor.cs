00d61122bd3845217028c51c9986b882b80c7f43
Xiunan <sam19951212@163.com>
2022-01-29 17:16:30 -0500

    map editor

--- Assets/Editor/MapManagerEditor.cs
+++ Assets/Editor/MapManagerEditor.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 using System.Reflection;
 using System;
 using System.IO;
+using UnityEngine.SceneManagement;
 
 public class MapManagerEditor : EditorWindow
 {
@@ -21,10 +22,14 @@ public class MapManagerEditor : EditorWindow
     const string LEVEL_DATA_PATH = "Assets/Data/";
     const string PREFAB_PATH = "Assets/Prefabs/";
 
+    Scene m_editorScene;
+
     string m_savedMapName;
 
-    Tilemap m_leftInteractiveLayer;
-    Tilemap m_rightInteractiveLayer;
+    Transform m_leftInteractiveLayer;
+    Transform m_rightInteractiveLayer;
+
+    LevelData m_loadLevelData;
 
     List<GameObject> prefabsInProject = new List<GameObject>();
 
@@ -32,7 +37,7 @@ public class MapManagerEditor : EditorWindow
     {
         if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
         {
-            EditorSceneManager.OpenScene("Assets/Scenes/MapEditing.unity");
+            m_editorScene = EditorSceneManager.OpenScene("Assets/Scenes/MapEditing.unity");
         }
 
         InitAllLayers();
@@ -48,17 +53,13 @@ public class MapManagerEditor : EditorWindow
         Selection.objects = previousSelection;
         foreach (var trans in selectedTransforms)
         {
-            Tilemap m_map = trans.GetComponent<Tilemap>();
-            if (m_map != null)
+            if (trans.name == "LeftInteractiveLayer")
             {
-                if (m_map.name == "LeftInteractiveLayer")
-                {
-                    m_leftInteractiveLayer = m_map;
-                }
-                else if (m_map.name == "RightInteractiveLayer")
-                {
-                    m_rightInteractiveLayer = m_map;
-                }
+                m_leftInteractiveLayer = trans;
+            }
+            else if (trans.name == "RightInteractiveLayer")
+            {
+                m_rightInteractiveLayer = trans;
             }
         }
     }
@@ -77,19 +78,98 @@ public class MapManagerEditor : EditorWindow
     void OnGUI()
     {
         //输入地图名字
-        m_savedMapName = EditorGUILayout.TextField("输入保存的地图文件名:", m_savedMapName);
-
-        if (GUILayout.Button("保存当前地图", GUILayout.Width(500), GUILayout.Height(30)))
+        
+        if(m_loadLevelData == null)
         {
+            GUILayout.Space(10);
+            m_savedMapName = EditorGUILayout.TextField("输入保存的地图文件名:", m_savedMapName);
+        }
+ 
+        if (m_loadLevelData == null && GUILayout.Button("保存当前地图", GUILayout.Width(500), GUILayout.Height(30)))
+        {
+            GUILayout.Space(10);
             if (string.IsNullOrEmpty(m_savedMapName))
             {
                 ShowNotification(new GUIContent("文件名不得为空"));
                 return;
             }
+
             Serialize();
             ShowNotification(new GUIContent("保存成功"));
         }
         
+        GUILayout.Space(10);
+        m_loadLevelData = (LevelData)EditorGUILayout.ObjectField("加载保存的关卡", m_loadLevelData, typeof(LevelData), false);
+        
+        GUILayout.Space(10);
+        if (m_loadLevelData != null && GUILayout.Button("加载", GUILayout.Width(500), GUILayout.Height(30)))
+        {
+            Clear();
+            m_savedMapName = m_loadLevelData.name;
+            Load(m_leftInteractiveLayer.transform);
+            Load(m_rightInteractiveLayer.transform);
+        }
+
+        if (m_loadLevelData != null && GUILayout.Button("保存修改", GUILayout.Width(500), GUILayout.Height(30)))
+        {
+            Serialize();
+        }
+
+        GUILayout.Space(10);
+        if (GUILayout.Button("清除地图元素", GUILayout.Width(500), GUILayout.Height(30)))
+        {
+            Clear();
+        }
+    }
+
+    void Clear()
+    {
+        for (int i = m_leftInteractiveLayer.transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = m_leftInteractiveLayer.GetChild(i);
+            DestroyImmediate(child.gameObject);
+        }
+
+        for (int i = m_rightInteractiveLayer.transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = m_rightInteractiveLayer.GetChild(i);
+            DestroyImmediate(child.gameObject);
+        }
+
+        EditorSceneManager.MarkSceneDirty(m_editorScene);
+        EditorSceneManager.SaveOpenScenes();
+    }
+
+    void Load(Transform map)
+    {
+        List<MapElement> elements = null;
+
+        if (map.name == "LeftInteractiveLayer")
+        {
+            elements = m_loadLevelData.LeftPlayerLevel;
+        }
+        else if (map.name == "RightInteractiveLayer")
+        {
+            elements = m_loadLevelData.RightPlayerLevel;
+        }
+
+        foreach (MapElement element in elements)
+        {
+            GameObject obj = Instantiate(prefabsInProject.Find(x => x.name == element.Prefab.name));
+            obj.name = obj.name.Replace("(Clone)", "");
+            if (map.name == "LeftInteractiveLayer")
+            {
+                obj.transform.parent = m_leftInteractiveLayer.transform;
+            }
+            else if (map.name == "RightInteractiveLayer")
+            {
+                obj.transform.parent = m_rightInteractiveLayer.transform;
+            }
+            obj.transform.localPosition = new Vector3(element.Row + 0.5f, 0.5f, element.Col + 0.5f);
+        }
+
+        EditorSceneManager.MarkSceneDirty(m_editorScene);
+        EditorSceneManager.SaveOpenScenes();
     }
 
     void Serialize()
@@ -107,7 +187,6 @@ public class MapManagerEditor : EditorWindow
         Selection.activeObject = asset;
         EditorUtility.SetDirty(asset);
         AssetDatabase.SaveAssets();
-        //AssetDatabase.ForceReserializeAssets();
     }
 
     void SerializeElements(LevelData asset, Transform map)
