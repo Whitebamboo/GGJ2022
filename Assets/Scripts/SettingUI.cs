66a36034190bc8ff24bdaebc23fcfa286d006918
Xiunan <sam19951212@163.com>
2022-01-30 17:44:54 -0500

    update setting UI

--- Assets/Scripts/SettingUI.cs
+++ Assets/Scripts/SettingUI.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class SettingUI : MonoBehaviour
 {
+    public float hintTime = 5f;
+
     public GameObject settingPage;
     public GameObject controlPage;
+
+    bool isBegin = true;
+
+    private void Start()
+    {
+        if (isBegin)
+        {
+            controlPage.SetActive(true);
+            isBegin = false;
+            StartCoroutine(CloseControlHint());
+        }
+    }
 
     public void OnOpenSetting()
     {
         settingPage.SetActive(!settingPage.activeSelf);
     }
 
+    public void OpenControlPage(bool active)
+    {
+        settingPage.SetActive(active);
+    }
 
+    IEnumerator CloseControlHint()
+    {
+        yield return new WaitForSeconds(hintTime);
+        controlPage.SetActive(false);
+    }
 }
