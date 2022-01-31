1a260fe950193bef904a1377fcc671f6607c8cca
Angela Zhang <angelaz1@andrew.cmu.edu>
2022-01-30 19:08:49 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -196,6 +196,7 @@ public class GameManager : CSingletonMono<GameManager>
                     currentLevel++;
                 } else {
                     EventBus.Broadcast(EventTypes.GameFinish);
+                    return;
                 }
 
                 if (musicManager) musicManager.PlayWinSFX();
